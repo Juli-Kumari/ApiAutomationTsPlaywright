e57bcc9f7582b76914560c26df04cf7e95ee2e86
Juli Kumari <julikumariece76@gmail.com>
2025-03-08 17:13:00 +0530

    response header

---
+++ tests/07_ReponseHeaderVerification.spec.ts
@@ -0,0 +1,18 @@
+import {expect, request, test} from '@playwright/test'
+
+test('Fetch & validate response header', async({request}) =>{
+    const response = await request.get('/booking/1')
+
+    const headerResponse = await response.headers()
+
+    console.log(headerResponse)
+    expect(headerResponse.server).toEqual('Cowboy')
+    expect(headerResponse['content-type']).toEqual('application/json; charset=utf-8')
+
+    console.log("*********************************")
+
+    const headerArrayValue = await response.headersArray()
+    console.log(headerArrayValue)
+    expect(headerArrayValue.length).toBe(11)
+
+})
